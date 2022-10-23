5166fd2624113eb691e4aa59889de7eb0caac086
Liam Naylor <liam@liamnaylor.com>
2022-10-22 19:02:38 -0700

    Fix bugs and added functionality to test vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+    let containsWord = false;
 
-module.exports = wordSearch
+    if (letters.length === 0) {
+        return containsWord;
+    }
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    let verticalJoin = [];
+
+    for (let i = 0; i < letters[0].length; i++) {
+        verticalJoin[i] = Array(letters.length).fill(i);
+      }
+    for (let i = 0; i < verticalJoin.length; i++) {
+        for (let x = 0; x < verticalJoin[i].length; x++) {
+            verticalJoin[i][x] = letters[x][i];
+        }
+    }
+    let verticalWords = verticalJoin.map(ls => ls.join(''));
+    for (let i = 0; i < verticalWords.length; i++) {
+        if (verticalWords[i].includes(word)) {
+            containsWord = true;
+        }
+    }
+    for (let i = 0; i < horizontalJoin.length; i++) { // For checking horizontal rows
+        if (horizontalJoin[i].includes(word)) {
+            containsWord = true;
+        }
+    }
+    return containsWord;
+};
+
+module.exports = wordSearch;
